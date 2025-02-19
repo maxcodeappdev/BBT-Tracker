65a7e5d7987f67795b44a188267a9e786f440bc0
Max Contreras <maxcodeappdev@gmail.com>
2025-02-19 08:48:05 -0700

    Initial Commit

---
+++ BBT Tracker/BBT_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBT_TrackerApp.swift
+//  BBT Tracker
+//
+//  Created by Max Contreras on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct BBT_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
